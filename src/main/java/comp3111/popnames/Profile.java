24cc252d019cbbcbac19861d0186d79ac9304029
hermanwpt <toto23630826@gmail.com>
2020-11-14 20:07:37 +0800

    Finished the getFreq in Profile class

--- src/main/java/comp3111/popnames/Profile.java
+++ src/main/java/comp3111/popnames/Profile.java
@@ -1,11 +1,23 @@
 package comp3111.popnames;
 
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import edu.duke.FileResource;
+
 public class Profile {
+	
     private int yearOfBirth;
     private String gender;
     private String name;
     private int rank;
     private int freq;
+    
+    // Helper function
+	public static CSVParser getFileParser(int year) {
+	     FileResource fr = new FileResource(String.format("dataset/yob%s.csv", year));
+	     return fr.getCSVParser(false);
+	}
 
     // Profile class constructor with name
     Profile(int yearOfBirth, String gender, String name) {
@@ -13,8 +25,23 @@ public class Profile {
         this.name = name;
         this.gender = gender;
         this.yearOfBirth = yearOfBirth;
-
-        this.freq = -1;     // yet to implement function to find frequency
+        
+        // initialize freq
+        boolean found = false;
+	    int oFreq = 0;
+	    for (CSVRecord rec : getFileParser(yearOfBirth)) {
+	        if (rec.get(1).equals(gender)) {
+	            if (rec.get(0).equals(name)) {
+	            	found = true;
+	            	oFreq = Integer.parseInt(rec.get(2));
+	             	break;
+	            }
+	        }
+	    }
+	    if (found)
+	    	this.freq = oFreq;
+	    else
+	    	this.freq = -1;
 
         this.rank = AnalyzeNames.getRank(yearOfBirth, name, gender);
     }
@@ -26,7 +53,24 @@ public class Profile {
         this.gender = gender;
         this.rank = rank;
 
-        this.freq = -1;     // yet to implement function to find frequency
+        // initialize freq
+        boolean found = false;
+	    int oFreq = 0;
+	    int currentRank = 0;
+	    for (CSVRecord rec : getFileParser(yearOfBirth)) {
+	        if (rec.get(1).equals(gender)) {
+	        	currentRank++;
+	            if (currentRank == rank) {
+	            	found = true;
+	            	oFreq = Integer.parseInt(rec.get(2));
+	             	break;
+	            }
+	        }
+	    }
+	    if (found)
+	    	this.freq = oFreq;
+	    else
+	    	this.freq = -1;
 
         this.name = AnalyzeNames.getName(yearOfBirth, rank, gender);
     }
