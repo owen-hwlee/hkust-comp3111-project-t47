9fa2499d1b045f2ee4448e82e3b49bdb58c516fc
hermanwpt <toto23630826@gmail.com>
2020-11-18 14:06:59 +0800

    Updating Task 3

--- src/test/java/comp3111/popnames/TestProfile.java
+++ src/test/java/comp3111/popnames/TestProfile.java
@@ -19,4 +19,32 @@ public class TestProfile {
 		Profile a = new Profile(1950, "F", "Velma");
 		assertTrue(a.getRank() == 267);
 	}
+	
+	@Test
+	public void testC()
+	{
+		Profile a = new Profile(2006, "F", "Jaslene");
+		assertTrue(a.getRank() == 18340);
+	}
+	
+	@Test
+	public void testD()
+	{
+		Profile a = new Profile(2005, "F", "Jenascia");
+		assertTrue(a.getRank() == 17595);
+	}
+	
+	@Test
+	public void testE()
+	{
+		Profile a = new Profile(2001, "F", "Lashanti");
+		assertTrue(a.getRank() == 16788);
+	}
+	
+	@Test
+	public void testF()
+	{
+		Profile a = new Profile(2002, "F", "Lashanti");
+		assertTrue(a.getRank() == 1703);
+	}
 }
